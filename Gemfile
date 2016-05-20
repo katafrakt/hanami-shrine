65ccb4e440e2efbee0f977aa22d58dd71f8af8dc
Pawel Swiatkowski <pawel.swiatkowski@fyber.com>
2016-05-20 17:28:27 +0200

    Make tests pass with Shrine 2.0

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in hanami-shrine.gemspec
 gemspec
+gem 'shrine', '>= 2.0.0'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,5 @@ require 'shrine/storage/file_system'
 
 Shrine.storages = {
   cache: Shrine::Storage::FileSystem.new(Dir.tmpdir),
-  store: Shrine::Storage::FileSystem.new("spec/tmp", subdirectory: "uploads")
+  store: Shrine::Storage::FileSystem.new("spec/tmp", prefix: "uploads")
 }
